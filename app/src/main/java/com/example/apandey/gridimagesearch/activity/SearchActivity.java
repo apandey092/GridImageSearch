dea8038fb294d4c8611c187db169d2adb4efa2d9
apandey <apandey@groupon.com>
2015-10-31 14:37:33 -0700

    add more user stories impl

--- app/src/main/java/com/example/apandey/gridimagesearch/activity/SearchActivity.java
+++ app/src/main/java/com/example/apandey/gridimagesearch/activity/SearchActivity.java
@@ -14,6 +14,7 @@ import android.widget.GridView;
 import com.example.apandey.gridimagesearch.R;
 import com.example.apandey.gridimagesearch.adapters.ImageResultsAdapter;
 import com.example.apandey.gridimagesearch.models.ImageResult;
+import com.example.apandey.gridimagesearch.utils.EndlessScrollListener;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
@@ -26,6 +27,13 @@ import java.util.ArrayList;
 import cz.msebera.android.httpclient.Header;
 
 public class SearchActivity extends AppCompatActivity {
+    public static int REQUEST_CODE = 10;
+    private String size;
+    private String type;
+    private String color;
+    private String siteFilter;
+    private int start = 0;
+
     EditText etQuery;
     //    Button btSearch;
     GridView gvResults;
@@ -36,6 +44,8 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+
         setUpViews();
 
     }
@@ -50,12 +60,41 @@ public class SearchActivity extends AppCompatActivity {
         gvResults.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-               Intent i = new Intent(getApplicationContext(), ImageDisplayActivity.class);
+                Intent i = new Intent(getApplicationContext(), ImageDisplayActivity.class);
                 ImageResult imageResult = imageResults.get(position);
                 i.putExtra("result", imageResult);
                 startActivity(i);
             }
         });
+        gvResults.setOnScrollListener(new EndlessScrollListener(8) {
+            @Override
+            public boolean onLoadMore(int page, int totalItemsCount) {
+                loadMore(page);
+                return true;
+            }
+        });
+
+
+
+    }
+
+    public void onComposeAction(MenuItem mi) {
+        Intent i = new Intent(this, AdvanceOptionsActivity.class);
+        startActivityForResult(i, REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_CODE && resultCode == RESULT_OK){
+//            i.putExtra("size", imageSize);
+//            i.putExtra("type", imageType);
+//            i.putExtra("color", imageColor);
+//            i.putExtra("sitefilter", siteFilter);
+            size = data.getStringExtra("size");
+            type = data.getStringExtra("type");
+            color = data.getStringExtra("color");
+            siteFilter = data.getStringExtra("sitefilter");
+        }
     }
 
     @Override
@@ -81,9 +120,26 @@ public class SearchActivity extends AppCompatActivity {
     }
 
     public void onImageSearch(View v) {
+       loadMore(0);
+    }
+
+    private void loadMore(int start){
         String query = etQuery.getText().toString();
 //        Toast.makeText(this, "Search for "+query, Toast.LENGTH_SHORT).show();
-        String searchUrl = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + query + "&rsz=8";
+        String searchUrl = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + query + "&rsz=8&start="+start;
+//        https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=fb&rsz=8&imgc=blue&imgsz=icon&imgtype=face&as_sitesearch=facebook.com
+        if(size != null && !size.isEmpty()){
+            searchUrl += "&imgsz="+size;
+        }
+        if(type != null && !type.isEmpty()){
+            searchUrl += "&imgtype="+type;
+        }
+        if(siteFilter != null && !siteFilter.isEmpty()){
+            searchUrl += "&as_sitesearch="+siteFilter;
+        }
+        if(color != null && !color.isEmpty()){
+            searchUrl += "&imgc="+color;
+        }
         AsyncHttpClient client = new AsyncHttpClient();
         client.get(searchUrl, new JsonHttpResponseHandler() {
             @Override
@@ -97,7 +153,6 @@ public class SearchActivity extends AppCompatActivity {
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-
                 Log.d("INFO", imageResults.toString());
             }
 
